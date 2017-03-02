8e8110b751c6df3b64651caa6e6a69d6692fa205
Derek Hildreth <derek@derekhildreth.com>
2017-03-02 19:39:38 +0000

    Implemented /temperatures route to return all pool temperatures at once

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var Pool = function (id) {
     this.vdcin = null;
     this.temperature = null;
     this.temperatureUnit = "F"; // Only unit supported at this time
+    this.error = null;
 }
 
 Pool.prototype.init = function(callback) {
@@ -32,23 +33,36 @@ Pool.prototype.init = function(callback) {
         callback(_this);
     })
     .catch(function(error) {
-        callback(error);
+        _this.temperature = null;
+        _this.temperatureUnit = null;
+        _this.error = error;
+        callback(_this);
     });
 };
 
 Pool.prototype.setTemperature = function () {
     var _this = this;
 
-    // TODO: Conversion from VDC to F ( or maybe this is done in getadc? )
-    var convertedTemperature = _this.vdcin / 1000;
-    _this.temperature = convertedTemperature;
+    /***
+    * y = m * x + b       vdc = m * temp + b
+    * x = (y - b) / m     temp = (vdc - b) / m
+    *
+    * Assuming 2000 mV is -40 and 10000 mV is 248 (max temperature range of T775M):
+    * temp = (vdc - 3111.11111111) / 27.7777777778
+    * 6000 mV ~= 103.9 F
+    ***/
+
+    var m = 27.778;
+    var b = 3111.111;
+
+    var convertedTemperature = (_this.vdcin - b) / m;
+    _this.temperature = convertedTemperature.toFixed(1);
 };
 
 Pool.prototype.getJson = function() {
     var _this = this;
     return JSON.stringify(_this);
 };
-
 
 var server = restify.createServer();
 server.pre(restify.pre.userAgentConnection());
@@ -59,9 +73,30 @@ server.pre(restify.pre.userAgentConnection());
 *******************************************************************************/
 server.get('/temperatures', function(req, res, next) {
 
-  res.send("NYI");
-  next();
+    numberOfPools = 3;
+    currentPool = 0;
+    allPools = [];
 
+    function get_pool() {
+
+        // We're done going through pools at this point, so return!
+        if (currentPool == numberOfPools) {
+            //console.log("allPools: ", allPools);
+            res.json(allPools);
+            next();
+        }
+        else {
+            var pool = new Pool(currentPool);
+
+            pool.init(function(response) {
+                allPools.push(response);
+                currentPool++; 
+                get_pool();
+            });
+        }
+    }
+
+    get_pool();
 });
 
 /*******************************************************************************
@@ -73,18 +108,11 @@ server.get('/temperatures/:pool', function(req, res, next) {
     var pool = new Pool(req.params.pool);
 
     pool.init(function(response) {
-
-	// If we didn't return an object, it must mean it's an error message.
-        if (typeof response === 'object') {
-            res.json(response);
-        }
-        else 
-            res.json({'error': response});
-
+        res.json(response);
         next();
     });
 });
 
 server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
